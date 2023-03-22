a2aaea9db29116cac254f934052fc6109c18d5b6
danju4rizzl <danjumashiwaju@gmail.com>
2023-03-22 18:27:57 +0200

    add preact + dark mode

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,3 +1,4 @@
+// This file configures the RSS data for the blog
 import rss, { pagesGlobToRssItems } from "@astrojs/rss"
 
 export async function get() {
@@ -7,7 +8,7 @@ export async function get() {
 		site: "https://astro-blogger-demo.netlify.app",
 		items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
 		customData: `<language>en-us</language>`,
-
+		// the stylesheet links the styles in the public folder
 		stylesheet: "/rss/styles.xsl"
 	})
 }
