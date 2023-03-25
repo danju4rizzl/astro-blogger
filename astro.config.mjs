847c1dd3fd0aba786692237f2a9bbd533e2972e7
danju4rizzl <danjumashiwaju@gmail.com>
2023-03-25 21:41:13 +0200

    added netlify cms admin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,78 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-import preact from "@astrojs/preact";
+import preact from "@astrojs/preact"
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
+
+import NetlifyCMS from "astro-netlify-cms"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
-});
+	integrations: [
+		preact(),
+		tailwind(),
+		NetlifyCMS({
+			config: {
+				backend: {
+					name: "git-gateway",
+					branch: "master"
+				},
+				media_folder: "public/images/uploads",
+				public_folder: "/images/uploads",
+				collections: [
+					// Content collections
+					// Define a blog post collection
+					{
+						name: "posts",
+						label: "Blog Posts",
+						folder: "src/pages/posts",
+						create: true,
+						delete: true,
+
+						disableIdentityWidgetInjection: true,
+						fields: [
+							{
+								name: "layout",
+								widget: "hidden",
+								label: "Layout",
+								default: "../../layouts/MarkdownPostLayout.astro "
+							},
+							{ name: "title", widget: "string", label: "Post Title" },
+							{ name: "pubDate", widget: "datetime", label: "Published Date" },
+							{
+								name: "description",
+								widget: "sting",
+								label: "Post Description"
+							},
+							{
+								name: "author",
+								widget: "sting",
+								label: "Post Author"
+							},
+							{
+								name: "image",
+								widget: "object",
+								label: "Post Author Pics",
+								fields: {
+									name: "url",
+									widget: "string",
+									label: "Author Pics URL"
+								},
+								fields: {
+									name: "alt",
+									widget: "string",
+									label: "Author Pics Alt"
+								}
+							},
+							{ name: "tags", widget: "list", label: "Post Tags" },
+							{ name: "cover", widget: "string", label: "Post Cover" },
+							{ name: "body", widget: "markdown", label: "Post Body" }
+						]
+					}
+				]
+			}
+		})
+	]
+})
