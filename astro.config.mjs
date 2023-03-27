43e1b1d26532d6e605ec303a42da392244f7b024
danju4rizzl <danjumashiwaju@gmail.com>
2023-03-27 12:10:22 +0200

    [WIP] fix cms issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,67 +12,66 @@ import NetlifyCMS from "astro-netlify-cms"
 export default defineConfig({
 	integrations: [
 		preact(),
-		tailwind(),
-		NetlifyCMS({
-			config: {
-				backend: {
-					name: "git-gateway",
-					branch: "master"
-				},
-				media_folder: "public/images/uploads",
-				public_folder: "/images/uploads",
-				collections: [
-					// Content collections
-					// Define a blog post collection
-					{
-						name: "posts",
-						label: "Blog Posts",
-						folder: "src/pages/posts",
-						create: true,
-						delete: true,
+		tailwind()
+		// DEEJAY To fix the issue with the package Changes this to config.toml
+		// NetlifyCMS({
+		// 	config: {
+		// 		backend: {
+		// 			name: "git-gateway",
+		// 			branch: "master"
+		// 		},
+		// 		media_folder: "public/images/uploads",
+		// 		public_folder: "/images/uploads",
+		// 		collections: [
+		// 			{
+		// 				name: "posts",
+		// 				label: "Blog Posts",
+		// 				folder: "src/pages/posts",
+		// 				create: true,
+		// 				delete: true,
 
-						disableIdentityWidgetInjection: true,
-						fields: [
-							{
-								name: "layout",
-								widget: "hidden",
-								label: "Layout",
-								default: "../../layouts/MarkdownPostLayout.astro "
-							},
-							{ name: "title", widget: "string", label: "Post Title" },
-							{ name: "pubDate", widget: "datetime", label: "Published Date" },
-							{
-								name: "description",
-								widget: "sting",
-								label: "Post Description"
-							},
-							{
-								name: "author",
-								widget: "sting",
-								label: "Post Author"
-							},
-							{
-								name: "image",
-								widget: "object",
-								label: "Post Author Pics",
-								fields: {
-									name: "url",
-									widget: "string",
-									label: "Author Pics URL"
-								},
-								fields: {
-									name: "alt",
-									widget: "string",
-									label: "Author Pics Alt"
-								}
-							},
-							{ name: "tags", widget: "list", label: "Post Tags" },
-							{ name: "cover", widget: "string", label: "Post Cover" },
-							{ name: "body", widget: "markdown", label: "Post Body" }
-						]
-					}
-				]
-			}
-		})
+		// 				disableIdentityWidgetInjection: true,
+		// 				fields: [
+		// 					{
+		// 						name: "layout",
+		// 						widget: "hidden",
+		// 						label: "Layout",
+		// 						default: "../../layouts/MarkdownPostLayout.astro "
+		// 					},
+		// 					{ name: "title", widget: "string", label: "Post Title" },
+		// 					{ name: "pubDate", widget: "datetime", label: "Published Date" },
+		// 					{
+		// 						name: "description",
+		// 						widget: "sting",
+		// 						label: "Post Description"
+		// 					},
+		// 					{
+		// 						name: "author",
+		// 						widget: "sting",
+		// 						label: "Post Author"
+		// 					},
+		// 					{
+		// 						name: "image",
+		// 						widget: "object",
+		// 						label: "Post Author Pics",
+		// 						fields: {
+		// 							name: "url",
+		// 							widget: "string",
+		// 							label: "Author Pics URL"
+		// 						},
+		// 						fields: {
+		// 							name: "alt",
+		// 							widget: "string",
+		// 							label: "Author Pics Alt"
+		// 						}
+		// 					},
+		// 					{ name: "tags", widget: "list", label: "Post Tags" },
+		// 					{ name: "cover", widget: "string", label: "Post Cover" },
+		// 					{ name: "body", widget: "markdown", label: "Post Body" }
+		// 				]
+		// 			}
+		// 		]
+		// 	}
+		// })
 	]
 })
